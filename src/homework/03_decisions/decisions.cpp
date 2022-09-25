7a8338d3221a1fd678691607d9b999485a318e8c
marcuson360hz <marcus.darensbourg@g.austincc.edu>
2022-09-24 23:27:22 -0500

    Homework 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,68 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+std::string get_letter_grade_using_if(int grade){
+
+    std::string letterGrade;
+
+    if (grade >= 90 && grade <= 100){
+
+        letterGrade = "A";
+    }
+    else if (grade >= 80 && grade <= 89){
+
+        letterGrade = "B";
+    }
+    else if (grade >= 70 && grade <= 79){
+
+        letterGrade = "C";
+    }
+    else if (grade >= 60 && grade <= 69){
+
+        letterGrade = "D";
+    }
+    else if (grade >= 00 && grade <= 59){
+
+        letterGrade = "F";
+    }
+    else{
+
+        letterGrade = "Number is out of range.";
+    }
+
+    return letterGrade;
+}
+
+std::string get_letter_grade_using_switch(int grade){
+
+    std::string letterGradeSwitch;
+
+    switch (grade)
+    {
+        case 90 ... 100:
+            letterGradeSwitch = "A";
+            break;
+
+        case 80 ... 89:
+            letterGradeSwitch = "B";
+            break;
+
+        case 70 ... 79:
+            letterGradeSwitch = "C";
+            break;
+
+        case 60 ... 69:
+            letterGradeSwitch = "D";
+            break;
+
+        case 0 ... 59:
+            letterGradeSwitch = "F";
+            break;
+
+        default:
+            letterGradeSwitch = "Number is out of range.";
+    }
+
+    return letterGradeSwitch;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,61 @@
 //write include statements
+#include <iostream>
+#include <string>
+#include "decisions.h"
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	auto yourGrade = 0;
+	int menuSelection;
+	
+	cout << "Welcome to the if/switch Grade Converter." << "\n";
+	cout << "MAIN MENU:\n\n" << "Option 1 - Calculate your letter grade using if\n" << "Option 2 - Calculate your letter grade using switch\n" << "Option 3 - Exit Program" << "\n\n";
+	cout << "Please select an option: ";
+	cin >> menuSelection;
+
+	if (menuSelection == 1){
+
+		cout << "\nYou have selected (if calculation)" << "\n";
+		cout << "Please enter your numerical grade: ";
+		cin >> yourGrade;
+
+		if (yourGrade >= 0 && yourGrade <= 100){
+
+			auto letterGradeIf = get_letter_grade_using_if(yourGrade);
+
+			cout << "Your letter grade is: " << letterGradeIf << "using the (if) calculation." << "\n\n";
+			cout << "Thank you for using the Grade Converter program. Goodbye!" << std::endl;
+		}
+		else{
+
+			cout << "The number is out of range." << "Terminating program." << std::endl;
+		}
+	}
+	else if (menuSelection == 2){
+
+		cout << "\nYou have selected (switch calculation)" << "\n";
+		cout << "Please enter your numerical grade: ";
+		cin >> yourGrade;
+
+		if (yourGrade >= 0 && yourGrade <= 100){
+
+			auto letterGradeSwitch = get_letter_grade_using_switch(yourGrade);
+
+			cout << "Your letter grade is: " << letterGradeSwitch << "using the (switch) calculation." << "\n\n";
+			cout << "Thank you for using the Grade Converter program. Goodbye!" << std::endl;
+		}
+		else{
+
+			cout << "The number is out of range." << "Terminating program." << std::endl;
+		}
+	}
+	else{
+
+		cout << "\nThank you for using the Grade Converter program. Goodbye!" << std::endl;
+	}
+
 	return 0;
 }
