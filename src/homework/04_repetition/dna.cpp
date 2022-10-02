014f6a8448de199df2f919140ed6f386ffb6f967
marcuson360hz <marcus.darensbourg@g.austincc.edu>
2022-10-01 23:43:34 -0500

    Homework 4

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include <iostream>
+#include "dna.h"
 
 //add function(s) code here
+int factorial(int num){
+
+    int sum = 1;
+
+    while (num > 0){
+
+        sum = sum * num;
+        num--;
+    }
+
+    return sum;
+}
+
+
+int gcd(int num1, int num2){
+
+    if (num1 == num2){
+
+        return num1;
+    }
+    else{
+
+        while (num1 != num2){
+
+            if ( num1 > num2){
+                num1 -= num2;
+            }
+            else{
+
+                num2 -= num1;
+            }
+        }
+    }
+}
