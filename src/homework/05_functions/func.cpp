922cb1e10a0dda8d143d0c33542e2dc28bc22a96
marcuson360hz <marcus.darensbourg@g.austincc.edu>
2022-10-09 00:29:22 -0500

    Homework 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,63 @@
-//add include statements
+#include <string>
+#include <iostream>
+#include "func.h"
 
-//add function code here
+double get_gc_content(const std::string& dna){
+
+    double gcPercentage;
+
+    for (int i = 0; i < dna.length(); i++){
+
+        if (dna[i] == 'C' || dna[i] == 'G'){
+
+            gcPercentage++;
+        }
+    }
+
+    double gcReturn = gcPercentage / dna.length();
+
+    return gcReturn;
+}
+
+std::string get_dna_complement(std::string dna){
+
+    std::string complementDNA = reverse_string(dna);
+
+    for (int i = 0; i < complementDNA.length(); i++){
+
+        switch (complementDNA[i]){
+
+            case 'A':
+                complementDNA[i] += 'T';
+                break;
+
+            case 'T':
+                complementDNA[i] += 'A';
+                break;
+
+            case 'C':
+                complementDNA[i] += 'G';
+                break;
+
+            case 'G':
+                complementDNA[i] += 'C';
+                break;
+
+        }
+        
+    }
+
+    return complementDNA;
+}
+
+std::string reverse_string(std::string dna){
+
+    std::string rna;
+
+    for (int i = dna.length() - 1; i >= 0; i--){
+
+        rna += dna[i];
+    }
+
+    return rna;
+}
